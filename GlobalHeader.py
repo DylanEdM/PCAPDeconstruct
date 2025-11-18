c1d84cd9fb65cb0ee51659e6c74eca5bf80b5900
DM <211373951+DylanEdM@users.noreply.github.com>
2025-11-18 01:33:07 +0000

    Header Length,Magic Number and Endianness done

--- GlobalHeader.py
+++ GlobalHeader.py
@@ -1,4 +1,18 @@
+import sys
+
 def main():
-    pass
+    with open("./ExamplePCAPs/CyberSecurity2026.pcap",'rb') as cap:
+        binary = cap.read() #read in binary
+    header = binary[0:24] #take header
+    endianness = ['little','big']
+    #Figure out if endianness of a file
+    #We can do this using the magic number
+    if header[0:4].hex() == ('a1b2c3d4' or 'a1b23c4d'): #big endian
+        magic_number = header[0:4].hex()
+        # if we get the magic number we know the endianness matches our system
+        endianness = [sys.byteorder]
+    else:
+        magic_number = header[0:4][::-1].hex()
+        endianness.remove(sys.byteorder)
 if __name__ == "__main__":
-    pass
+    main()
