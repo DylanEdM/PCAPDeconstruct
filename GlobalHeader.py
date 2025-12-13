baf04388d2b10a726984674898bba0ee449efb93
DM <211373951+DylanEdM@users.noreply.github.com>
2025-12-13 17:08:36 +0000

    All done! Could move to OOP.

--- GlobalHeader.py
+++ GlobalHeader.py
@@ -1,18 +1,30 @@
-import sys
-
+import linklayer
 def main():
     with open("./ExamplePCAPs/CyberSecurity2026.pcap",'rb') as cap:
         binary = cap.read() #read in binary
-    header = binary[0:24] #take header
-    endianness = ['little','big']
+    _endianness = {'little':-1,'big':1}
+    endianness = ''
     #Figure out if endianness of a file
     #We can do this using the magic number
-    if header[0:4].hex() == ('a1b2c3d4' or 'a1b23c4d'): #big endian
-        magic_number = header[0:4].hex()
+    if binary[0:4].hex() == ('a1b2c3d4' or 'a1b23c4d'): # if true, we know big endian
         # if we get the magic number we know the endianness matches our system
-        endianness = [sys.byteorder]
+        endianness = 'big'
+    elif binary[0:4][::-1].hex() == ('a1b2c3d4' or 'a1b23c4d'):
+        endianness = 'little'
     else:
-        magic_number = header[0:4][::-1].hex()
-        endianness.remove(sys.byteorder)
+        raise ValueError('Invalid Magic Number, file unreadable')
+    magic_number = binary[0:4][::_endianness[endianness]].hex()
+    major_version = binary[4:6].hex().strip('0')
+    minor_version = binary[6:8].hex().strip('0')
+    if major_version != '2' or minor_version != '4':
+        raise TypeError(f'Unsupported file version {major_version}.{minor_version}: Unable to parse file')
+    snap_len = int(binary[16:20][::_endianness[endianness]].hex(),16)
+    link_type = linklayer.shortName(int(binary[20:22][::_endianness[endianness]].hex(),16))
+    return endianness,magic_number,major_version,minor_version,snap_len,link_type
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except ValueError as e:
+        print(e)
+    except TypeError as e:
+        print(e)
